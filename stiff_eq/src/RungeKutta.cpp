cc28c55758d8f23a357c981bdafb83cbe6a0e27a
unknown <korovin.va@phystech.edu>
2020-03-31 01:20:19 +0300

    double->float

--- stiff_eq/src/RungeKutta.cpp
+++ stiff_eq/src/RungeKutta.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 #include <array>
 #include <cmath>
+#include <iostream>
 #include "video.h"
 #define PRECISION 1
 
 PointApply CalculatePoint(std::array<Point, 3> PrevPoints, float IntegrationSegment, float lastX)
 {
-	float h = 0.0003f;
+	float h = 0.0002f;
 
 	if (PrevPoints[0].getLocation().x >= 1000)
 	{
@@ -23,21 +24,21 @@ PointApply CalculatePoint(std::array<Point, 3> PrevPoints, float IntegrationSegm
 		return ret;
 	}
 
-	float Y1 = 0.f;
-	float Y2 = 0.f;
-	float Y3 = 0.f;
-	float t  = 0.f;
+	double Y1 = 0.f;
+	double Y2 = 0.f;
+	double Y3 = 0.f;
+	static double t  = 0.f;
 
-	float prevt  = PrevPoints[0].getLocation().x;
-	float PrevY1 = PrevPoints[0].getLocation().y;
-	float PrevY2 = PrevPoints[1].getLocation().y;
-	float PrevY3 = PrevPoints[2].getLocation().y;
+	//float prevt  = PrevPoints[0].getLocation().x;
+	double PrevY1 = PrevPoints[0].getLocation().y;
+	double PrevY2 = PrevPoints[1].getLocation().y;
+	double PrevY3 = PrevPoints[2].getLocation().y;
 
-	float LastY1 = 0.f;
-	float LastY2 = 0.f;
-	float LastY3 = 0.f;
+	double LastY1 = 0.f;
+	double LastY2 = 0.f;
+	double LastY3 = 0.f;
 //Prognosis
-	t = prevt + h;
+	t += h;
 	Y1 = PrevY1 + h * (-0.04f * PrevY1 + 10000 * PrevY2 * PrevY3);
 	Y2 = PrevY2 + h * (0.04f * PrevY1 - 10000 * PrevY2 * PrevY3 - 3 * 10000000 * PrevY2 * PrevY2);
 	Y3 = PrevY3 + h * (3 * 10000000 * PrevY2 * PrevY2);
@@ -59,8 +60,9 @@ PointApply CalculatePoint(std::array<Point, 3> PrevPoints, float IntegrationSegm
 	bool isapply = false;
 	if (t - lastX > IntegrationSegment / 1000.f)
 	{
+		std::cout << LastY1 << std::endl;
 		isapply = true;
-		t = float(int(t) + 1.f);
+		t = float(int(lastX) + 1.f);
 	}
 
 	PointApply ret =
